bbb238c77f741d0cd1fa0cee1cb41b6bb24adace
alswns6327 <alswns6327@naver.com>
2026-02-19 23:08:08 +0900

    password 입력시 한글 체크 공통 로직 추가

--- number_guess/common.c
+++ number_guess/common.c
@@ -4,3 +4,10 @@ void clear_input_buffer() {
 	int c;
 	while ((c = getchar()) != '\n' && c != EOF);
 }
+
+int has_non_ascii(char* str) {
+	while (*str) {
+		if ((unsigned)*str++ > 127) return 1;
+	}
+	return 0;
+}
--- number_guess/common.h
+++ number_guess/common.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void clear_input_buffer();
+int has_non_ascii(char*);
